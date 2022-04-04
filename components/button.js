9c59c5ebc6648cf42260d17454b92309e78f1c4f
hymos-two <newdategettime@gmail.com>
2022-04-04 16:25:28 +0700

    add another section

--- components/button.js
+++ components/button.js
@@ -4,7 +4,6 @@ export function Button (props) {
   const {text,icon,rotateIcon,className} =  props
 
   const btnClass = `block flex gap-4 lg:gap-1 p-3.5 bg-accent hover:bg-zinc-200 ${className}`
-  const divClass = `w-5 h-5 rotate-${rotateIcon}`
   return (
     <button className={btnClass}>
       {text && 
@@ -13,7 +12,7 @@ export function Button (props) {
       </span>
       }
       {icon && 
-      <div className={divClass}>
+      <div className="w-5 h-5" style={{transform:`rotate(${rotateIcon}deg)`}}>
         <Image src={icon} alt="icon"/>
       </div>
       }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {createRef} from 'react'
+import {useState,useEffect} from 'react'
 import Image from 'next/image'
 import HTMLHead from '../components/htmlHead'
 import PageCover from '../components/pageCover'
@@ -18,6 +18,9 @@ export default function Home() {
     <Section1Cover />
     <Section2Quote />
     <Section3Events />
+    <Section4Member />
+    <Section5Snapshot />
+    <Section6Yearbook />
     </>
   )
 }
@@ -43,7 +46,7 @@ function Section1Cover () {
           </div>
         </div>
       </div>
-      <Button icon={arrow} rotateIcon="180" className="absolute right-4 -bottom-6"/>
+      <Button icon={arrow} rotateIcon={180} className="absolute right-4 -bottom-6"/>
     </PageCover>
   )
 }
@@ -87,7 +90,7 @@ function Section3Events () {
               Events
             </h1>
             <span className="block flex justify-start items-center">
-              <span className="block w-12 h-0.5 bg-accent"></span>
+              <span className="block w-12 lg:w-24 h-0.5 bg-accent"></span>
             </span>
           </div>
         </Grid>
@@ -103,7 +106,7 @@ function Section3Events () {
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam, quis nostrud 
               </p>
-              <Button text="Lihat Selengkapnya" icon={arrow} rotateIcon="90" className="mt-16"/>
+              <Button text="Lihat Selengkapnya" icon={arrow} rotateIcon={90} className="mt-16"/>
             </div>
           </div>
         </Grid>
@@ -115,56 +118,158 @@ function Section3Events () {
 
 function EventsImage (props) {
   const {className,width} = props
+  const [widthSize,setWidthSize] = useState(0)
+
+  useEffect(()=>{
+    setWidthSize(()=>{
+      if (width == undefined) { return 0 } else
+      if (width >= 1280) { return (1140 - 36) * 8 / 12 / 4 } else 
+      if (width >= 1024) { return (960 - 36) * 8 / 12 / 4 } else 
+      { return width / 4 }
+    })
+  },[width])
+
+  const divImgStyle = (offsetTop) => ({
+    width:widthSize,
+    height:widthSize*3/2,
+    top:widthSize*3/2*offsetTop/100,
+  })
   const containerClass = `flex gap-2 sm:gap-3 ${className}`
-  function widthSize () {
-    if (width >= 1280) { return (1140 - 36) * 8 / 12 / 4 } 
-    else if (width >= 1024) { return (960 - 36) * 8 / 12 / 4 } 
-    else { return width / 4 }
-  }
   return (
-    <div className={containerClass} style={{height:widthSize()*3/2*130/100}}>
-      <div 
-        className="relative" 
-        style={{
-          width:widthSize(),
-          height:widthSize()*3/2,
-          top:widthSize()*3/2*5/100,
-          }}
-        >
-        <Image src={background} layout="fill" objectFit="cover"/>
-      </div>
-      <div 
-        className="relative" 
-        style={{
-          width:widthSize(),
-          height:widthSize()*3/2,
-          top:widthSize()*3/2*30/100, 
-          }}
-        >
-        <Image src={background} layout="fill" objectFit="cover"/>
-      </div>
-      <div 
-        className="relative" 
-        style={{
-          width:widthSize(),
-          height:widthSize()*3/2,
-          top:widthSize()*3/2*0, 
-          }}
-        >
-        <Image src={background} layout="fill" objectFit="cover"/>
-      </div>
-      <div 
-        className="relative" 
-        style={{
-          width:widthSize(),
-          height:widthSize()*3/2,
-          top:widthSize()*3/2*15/100, 
-          }}
-        >
-        <Image src={background} layout="fill" objectFit="cover"/>
-      </div>
+    <div className={containerClass} style={{height:widthSize*3/2*130/100}}>
+      {[5,30,0,15].map((value,i) => (
+        <div className="relative" style={divImgStyle(value)} key={i}>
+          <Image src={background} layout="fill" objectFit="cover" placeholder="blur"/>
+        </div>
+      ))
+      }
     </div>
   )
 }
 
 
+// ========================================
+// ========== Section 4 Member 
+// ========================================
+
+function Section4Member (props) {
+  return (
+    <Section className="bg-black-200 pt-20 lg:py-24" height="100%">
+      <Container>
+        <Grid cols={1}>
+          <div className="col-span-1 flex gap-2">
+            <h1 className="text-mono-m text-2xl md:text-[28px] text-white/90">
+              Member
+            </h1>
+            <span className="block flex justify-start items-center">
+              <span className="block w-12 lg:w-24 h-0.5 bg-accent"></span>
+            </span>
+          </div>
+        </Grid>
+      </Container>
+
+      <Container>
+        <Grid className="lg:grid-flow-col-dense">
+          <div className="bg-gray-300 col-span-4 md:col-span-8 lg:col-span-7 lg:col-start-6">
+            <div className="h-[180px] lg:h-[360px] w-full bg-red-200"></div>
+          </div>
+
+          <div className="grid place-items-center col-span-4 md:col-span-8 lg:col-span-4 lg:pr-16">
+            <div className="grid place-items-center px-3 sm:px-12 lg:px-0 lg:block">
+              <p className="text-sans text-sm leading-6 text-white/80 text-center lg:text-left">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud 
+              </p>
+              <Button text="Lihat Selengkapnya" icon={arrow} rotateIcon={90} className="mt-16"/>
+            </div>
+          </div>
+        </Grid>
+      </Container>
+    </Section>
+  )
+}
+
+// ========================================
+// ========== Section 5 Snapshot 
+// ========================================
+
+function Section5Snapshot (props) {
+  return (
+    <Section className="bg-black-300 pt-20 lg:py-24" height="100%">
+      <Container>
+        <Grid cols={1}>
+          <div className="col-span-1 flex gap-2">
+            <h1 className="text-mono-m text-2xl md:text-[28px] text-white/90">
+              Snapshot
+            </h1>
+            <span className="block flex justify-start items-center">
+              <span className="block w-12 lg:w-24 h-0.5 bg-accent"></span>
+            </span>
+          </div>
+        </Grid>
+      </Container>
+
+      <Container>
+        <Grid className="lg:grid-flow-col-dense">
+          <div className="bg-gray-300 col-span-4 md:col-span-8 lg:col-span-7 lg:col-start-6">
+            <div className="h-[180px] lg:h-[360px] w-full bg-red-200"></div>
+          </div>
+
+          <div className="grid place-items-center col-span-4 md:col-span-8 lg:col-span-4 lg:pr-16">
+            <div className="grid place-items-center px-3 sm:px-12 lg:px-0 lg:block">
+              <p className="text-sans text-sm leading-6 text-white/80 text-center lg:text-left">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud 
+              </p>
+              <Button text="Lihat Selengkapnya" icon={arrow} rotateIcon={90} className="mt-16"/>
+            </div>
+          </div>
+        </Grid>
+      </Container>
+    </Section>
+  )
+}
+
+// ========================================
+// ========== Section 6 Yearbook
+// ========================================
+
+function Section6Yearbook (props) {
+  return (
+    <Section className="bg-black-200 pt-20 lg:py-24" height="100%">
+      <Container>
+        <Grid cols={1}>
+          <div className="col-span-1 flex gap-2">
+            <h1 className="text-mono-m text-2xl md:text-[28px] text-white/90">
+              Yearbook
+            </h1>
+            <span className="block flex justify-start items-center">
+              <span className="block w-12 lg:w-24 h-0.5 bg-accent"></span>
+            </span>
+          </div>
+        </Grid>
+      </Container>
+
+      <Container>
+        <Grid className="lg:grid-flow-col-dense">
+          <div className="bg-gray-300 col-span-4 md:col-span-8 lg:col-span-7 lg:col-start-6">
+            <div className="h-[180px] lg:h-[360px] w-full bg-red-200"></div>
+          </div>
+
+          <div className="grid place-items-center col-span-4 md:col-span-8 lg:col-span-4 lg:pr-16">
+            <div className="grid place-items-center px-3 sm:px-12 lg:px-0 lg:block">
+              <p className="text-sans text-sm leading-6 text-white/80 text-center lg:text-left">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud 
+              </p>
+              <Button text="Lihat Selengkapnya" icon={arrow} rotateIcon={90} className="mt-16"/>
+            </div>
+          </div>
+        </Grid>
+      </Container>
+    </Section>
+  )
+}
